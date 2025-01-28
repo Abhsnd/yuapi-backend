c3f0dddf2aa0a5a91640762ed3ec1ae7ab130f1f
wiho <winhoncoder@gmail.com>
2025-01-28 23:48:17 +0800

    interfaceInfo function finished 250128

---
+++ src/main/java/com/yupi/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.project.mapper;
+
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author LENOVO
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2025-01-28 21:41:31
+* @Entity com.yupi.project.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
